a509cc3def6e73792c1ab7fbd302eb47d94947da
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2023-04-18 22:57:57 +0700

    Add files via upload

---
+++ etc/path.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HOME_THEPAK="${0%/*}"
+banner="etc/colors.sh"
+header="etc/header.conf" #this are doesnt needed anymore
+pak_folder="/mnt/b/thepak/pak_folder"
+out_folder="${pak_folder%/*}/out_folder"
