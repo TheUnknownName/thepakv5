eac384c803acd0285a02658f1c73aea398883180
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2023-04-22 08:54:47 +0700

    Update path.sh

--- etc/path.sh
+++ etc/path.sh
@@ -2,5 +2,5 @@
 HOME_THEPAK="${0%/*}"
 banner="etc/colors.sh"
 header="etc/header.conf" #this are doesnt needed anymore
-pak_folder="/mnt/b/thepak/pak_folder"
+pak_folder="/sdcard/download/pak_folder"
 out_folder="${pak_folder%/*}/out_folder"
